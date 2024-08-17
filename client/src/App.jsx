8dea7cfb2a22e1e3e3368062fbe90a61b5dfbbef
NishantPatelxx <138348371+NishantPatelxx@users.noreply.github.com>
2024-08-17 12:27:53 +0530

    #Added pages and routes on frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {BrowserRouter , Router , Route , Routes} from "react-router-dom"
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Projects from './pages/Projects'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      <Route path='/signin' element={<Signin/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/project' element={<Projects/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

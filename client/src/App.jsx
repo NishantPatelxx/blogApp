7ac63ff2cd5388cebdca9172fc7e2b45163ae2a4
NishantPatelxx <138348371+NishantPatelxx@users.noreply.github.com>
2024-08-18 19:07:23 +0530

    #header section need improved css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/signup' element={<Signup/>}></Route>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js", // Add this line
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'), // Add this line
+  ],
 }
